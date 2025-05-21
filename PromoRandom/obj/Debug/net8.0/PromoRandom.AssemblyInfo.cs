1ea7c762558b8123b2d66f579c15f4eead773f7d
murod <murod@ELITE-BOOK>
2025-05-21 15:01:29 +0500

    xuy

--- PromoRandom/obj/Debug/net8.0/PromoRandom.AssemblyInfo.cs
+++ PromoRandom/obj/Debug/net8.0/PromoRandom.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PromoRandom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b973f97d80f6784db93bcb22bb4812a3c0f7ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b7c6310ec3adaaa1469c4d3234d6e9d024271d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PromoRandom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PromoRandom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
