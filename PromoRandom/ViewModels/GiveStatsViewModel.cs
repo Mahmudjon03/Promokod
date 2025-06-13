da9e483e659519fc82494a05b7c956c1d6a1f910
murod <murod@ELITE-BOOK>
2025-06-13 13:35:05 +0500

    hay k

---
+++ PromoRandom/ViewModels/GiveStatsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PromoRandom.ViewModels
+{
+    public class WinnerStatModel
+    {
+        public string GiveawayName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public List<PrizeWinner> Winners { get; set; } = new();
+
+    }
+}
